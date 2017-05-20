2d258d0031027e43fd7469d7db13f7406527d8d7
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2017-05-20 07:15:31 +0530

    Add files via upload

---
+++ avg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asishcsharp
+{
+    class avg
+    {
+        static void Main()
+        {
+            float[] a;
+            a = new float[100];
+            int i, n;double sum = 0.0, avg;
+            Console.WriteLine("enter no.of elements");
+            n = Int32.Parse(Console.ReadLine());
+            for(i=0;i<n;i++)
+            {
+                Console.WriteLine("enter elements", i + 1);
+                a[i]=Int32.Parse(Console.ReadLine());
+                sum = sum + a[i];
+            }
+            avg = sum / n;
+            Console.WriteLine("avg={0}", avg);
+
+
+
+
+
+        }
+    }
+}
