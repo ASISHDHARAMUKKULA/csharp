ce9bc94918af275d44e1ec07afc6c4b7054a9ad3
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2017-05-20 07:19:40 +0530

    Add files via upload

---
+++ product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asishcsharp
+{
+    class product
+    {
+        private string name;
+        private int price;
+        public product(string name,int price)
+        {
+            this.price = price;
+            this.name = name;
+           
+
+        }
+        public void Print()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(price);
+        }
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                this.price = value;
+            }
+        }
+        public int Netprice
+        {
+            get
+            {
+                return price + price * 12 / 100;
+            }
+           
+        }
+    }
+    class testproduct
+    {
+        static void Main()
+        {
+            product p;
+            p = new product("Iphone",70000);
+            p.Print();
+            p.Price = 50000;
+            Console.WriteLine(p.Price);
+            Console.WriteLine(p.Netprice);
+        }
+    }
+}
