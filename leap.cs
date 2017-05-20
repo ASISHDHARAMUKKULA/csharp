a340c1382eb96e96b961f31d76460092ead1fc8c
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2017-05-20 07:18:48 +0530

    Add files via upload

---
+++ leap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asishcsharp
+{
+    class leap
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter a  year");
+            int year = Int32.Parse(Console.ReadLine());
+            if(year%400==0)
+            {
+                Console.WriteLine("Leapyear");
+            }
+            else
+            {
+                Console.WriteLine("not a leap year");
+            }
+        }
+        
+
+    }
+}
