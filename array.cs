795f73c7cdcb97401fbebc6469e2d93df27471fa
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2017-08-06 11:51:11 +0530

    Create array.cs

---
+++ array.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asishcsharp
+{
+    class array
+    {
+        public static void Main()
+        {
+            int[] intarray;
+            intarray = new int[5] { 1, 2, 3, 4 ,5};
+            Console.WriteLine(intarray[0]);
+            Console.WriteLine(intarray[1]);
+            Console.WriteLine(intarray[2]);
+            foreach(int b in intarray)
+            {
+                Console.WriteLine(b);
+            }
+            int[,] numbers = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            foreach(int c in numbers)
+            {
+                Console.WriteLine(c);
+            }
+           
+        }
+        
+
+    }
+}
